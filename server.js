3c78a9ca903881416e8b0fd14298de84d9d625c9
Okera Johnson <okerajohnson@gmail.com>
2025-09-05 09:23:41 -0400

    keeping

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Api server running on port ${PORT}`);
 });
+
+let cards = [
+    { id: 1, cardName: 'Mihawk', set: 'op-12', rarity: 'SR', price: '100'},
+    { id: 2, cardName: 'Zoro', set: 'op-10', rarity: 'R', price: '110'},
+    { id: 3, cardName: 'Rayleigh', set: 'op-01', rarity: 'SEC', price: '9'},
+    { id: 4, cardName: 'Monkey', set: 'op-07', rarity: 'UC', price: '7'}
+]
+
+let nextId = 5;
